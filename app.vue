63f25be5e7fb2f690223e9735c0fb73b1ac7035c
Gustavo Diniz <faculdogu@gmail.com>
2024-06-28 11:18:16 +0000

    header change

---
+++ app.vue
@@ -0,0 +1,59 @@
+<script setup>
+
+</script>
+<template>
+<nav class="navbar navbar-expand bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand text-white mx-3" href="#">
+      <h4 class="m-auto">
+        <img src="@/assets/img/menin.png" height="28px" class="mx-2 mb-1">Menin
+      </h4>
+    </a>
+    <div class="navbar-collapse" id="navbarText">
+      <ul class="navbar-nav">
+        <li class="nav-text">
+          <a class="nav-link" href="#">
+            <router-link class="links-menu px-1" to="/">Gerador I</router-link>
+          </a>
+        </li>
+        <li class="nav-text">
+          <a class="nav-link" href="#">
+            <router-link class="links-menu px-1" to="/tela2">Gerador II</router-link>
+          </a>
+        </li>
+        <li class="nav-text">
+          <a class="nav-link" href="#">
+            <router-link class="links-menu px-1" to="/tela3"><i class="bi bi-exclamation-triangle"></i></router-link>
+          </a>
+        </li>
+      </ul>
+      <span class="navbar-text text-white">
+      </span>
+    </div>
+  </div>
+</nav>
+
+
+
+  <div class="main">
+    <router-view>
+    </router-view>
+  </div>
+
+  <footer class="d-flex py-lg-3 py-2 w-100 text-light bg-dark position-fixed bottom-0">
+    <div class="col-12 d-flex align-items-center justify-content-center">
+      <img class="mx-2" src="@/assets/img/menin.png" width="30" ></img>
+      <span>© {{ new Date().getFullYear() }} Menin Engenharia | Gustavo Diniz,
+        All Rights Reserved.</span>
+    </div>
+  </footer>
+
+</template>
+
+<style scoped>
+a {
+  text-decoration: none;
+  color: white;
+}
+
+</style>
---
+++ src/assets/components/Header.vue
@@ -0,0 +1,41 @@
+<script setup>
+
+</script>
+
+<template> 
+
+<nav class="navbar navbar-expand-lg bg-secondary">
+  <div class="container-fluid">
+    <img src="../assets/img/menin.png" alt="" width="40">
+    <a class="fw-bolder mx-2 navbar-brand" href="#">Menin</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Disparos
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Gerador Manual</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Gerador com Pesquisa</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+</template>
+
+<style scoped>
+
+</style>
